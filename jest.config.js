d97dbc9e2cb3ea4bd348654a5675db06e72a6a40
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2025-10-10 14:29:24 +0000

    fix: arrumando testes

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
-/** @type {import('jest').Config} */ 
-const config = { 
-  testEnvironment: "jest-environment-jsdom", 
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], 
-  moduleNameMapper: { 
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy", 
-  }, 
-}; 
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+};
+
 module.exports = config;
---
+++ src/pages/index.jsx
@@ -0,0 +1,6 @@
+
+const Home = () => {
+  return <h1>Bem-vindo à Pousada Pedra Furada</h1>;
+};
+
+export default Home;
