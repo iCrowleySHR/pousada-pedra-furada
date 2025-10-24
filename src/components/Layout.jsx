99179a3d1607c2c00d495b18689ecffab7e4cf54
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2025-10-24 16:51:26 +0000

    “feat: Implementado o Layout.jsx e renomeado o arquivo Header.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Header from "./Header";
+
 export default function Layout({ children }) {
-    return (
-        <div>
-            <Header />
-            <main style={{ padding: "1rem" }}>{children}</main>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <main style={{ padding: "1rem" }}>
+        {children}
+      </main>
+      <footer
+        style={{
+          background: "#234",
+          color: "#fff",
+          textAlign: "center",
+          padding: "0.5rem",
+          marginTop: "1rem",
+        }}
+      >
+        © {new Date().getFullYear()} Pousada Pedra Furada
+      </footer>
+    </div>
+  );
 }
